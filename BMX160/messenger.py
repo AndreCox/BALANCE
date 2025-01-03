0f102292aa1ca1c0f7f3a3827d769bb378f06576
Andre Cox <arc.xoc@gmail.com>
2025-01-03 09:01:02 -0800

    BMX160 messenger

---
+++ BMX160/messenger.py
@@ -0,0 +1,37 @@
+import lcm
+import bmx160 as imu
+import sys
+import time
+sys.path.append('../')
+
+import LCM.imu_lcm as imu_lcm
+
+lc = lcm.LCM()
+bmx160 = imu.BMX160()
+
+while True:
+    try:
+        print("Initializing BMX160...")
+        bmx160.begin()
+        break
+    except:
+        print("BMX160 not found or there was an error initializing the sensor. Retrying...")
+        time.sleep(1)
+        pass
+
+while True:
+    all_sensor_data = bmx160.get_all_data()
+    mag = all_sensor_data[0:3]
+    gyro = all_sensor_data[3:6]
+    accl = all_sensor_data[6:9]
+
+    try:
+        msg = imu_lcm.imu_t()
+        msg.mag = mag
+        msg.gyro = gyro
+        msg.accl = accl
+
+        lc.publish("SENSOR_DATA", msg.encode())
+        print(f"Published: {msg}")
+    except Exception as e:
+        print(f"Error publishing data: {e}")
